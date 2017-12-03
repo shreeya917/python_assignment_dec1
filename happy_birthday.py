74d40dcedce04bd4fcd676d2b128bc4aa7f51924
shreeya917 <shreeyaregmi6@gmail.com>
2017-12-03 18:43:08 +0545

    Completed

---
+++ happy_birthday.py
@@ -0,0 +1,122 @@
+Conversation
+opened.
+1
+read
+message.
+
+Skip
+to
+content
+Using
+Deerwalk
+Institute
+of
+Technology
+Mail
+with screen readers
+Search
+
+shreya
+
+Click
+here
+to
+enable
+desktop
+notifications
+for Deerwalk Institute of Technology Mail.Learn more  Hide
+Mail
+COMPOSE
+Labels
+Inbox
+Starred
+Sent
+Mail
+Drafts
+More
+Hangouts
+
+Move
+to
+Inbox
+More
+2
+of
+about
+97
+
+Print
+all
+In
+new
+window
+(no
+subject)
+Inbox
+x
+
+Shreya
+Sapkota
+AttachmentsNov
+29(4
+days
+ago)
+
+to
+me
+Regards,
+Shreya
+Sapkota
+3
+Attachments
+
+Click
+here
+to
+Reply or Forward
+Using
+2.81
+GB
+Program
+Policies
+Powered
+by
+Google
+Last
+account
+activity: 16
+minutes
+ago
+Details
+
+
+# 03. Happy Birthday Song for a Friend
+
+def yes():
+    print("Happy Birthday to", name)
+    print("Happy Birthday to You\nMay God Bless You\nCheers!", name)
+
+
+def no():
+    print("Never Mind Then...")
+
+
+name = input("What is your name?\n")
+
+print("So, Is it your birthday today?\n")
+
+ans = input("yes or no?\n")
+
+if (ans == 'yes'):
+    yes()
+
+else:
+    no()
+
+03
+happy_birthday_song.py
+Open
+with Google Docs
+Displaying
+03
+happy_birthday_song.py.
